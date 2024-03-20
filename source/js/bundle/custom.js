5e00f0677e4e5859d99c88c87a33288d42e20d65
Lolinelyf <lolinelyf@mail.ru>
2024-03-20 15:09:23 +0300

    change fonts

--- source/js/bundle/custom.js
+++ source/js/bundle/custom.js
@@ -34,7 +34,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   window.addEventListener('click', (event) => {
-    event.stopPropagation();
     if (event.target === overlay) {
       toggleMenu();
     }
